3dd0b3ab6952d37b32996ac19e2a24f8b2772616
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-11-24 12:42:08 +0100

    Source and binary build options, only target amd64 arch.

--- build-nigthly-deb-from-master.sh
+++ build-nigthly-deb-from-master.sh
@@ -2,18 +2,29 @@
 
 set -ex
 
-export DEBFULLNAME="bot"
-export DEBEMAIL="bot@address.local"
+# Use defaults if not provided in environment
+: ${DEBFULLNAME:=bot}
+: ${DEBEMAIL:=bot@address.local}
+: ${SIGN_FLAGS:=-us -uc}
+: ${DISTRIB:=UNRELEASED}
+: ${BUILD_OPTS:=-F}
+
+export DEBFULLNAME DEBEMAIL
+
+if [ -f "${PUBKEY_FILE}" ]; then
+    chmod 700 $HOME/.gnupg
+    gpg --import ${PUBKEY_FILE}
+fi
 
 PROJECT=ngdevkit-toolchain
 UPSTREAM_VERSION=$(git grep VERSION origin/master:Makefile | sed -ne 's/.*=\(.*\)$/\1/p')
 read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" origin/master)
 DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
 
-dch -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
+dch -D ${DISTRIB} -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
 git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ origin/master | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 cd ${PROJECT}-${DEB_VERSION}
 cp -a ../debian .
 yes | mk-build-deps --install --remove
-dpkg-buildpackage -rfakeroot -us -uc
+dpkg-buildpackage -rfakeroot ${BUILD_OPTS} ${SIGN_FLAGS}
