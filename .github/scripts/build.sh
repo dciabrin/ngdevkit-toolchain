6aeb9db73b1391134d64707865195e1373b85f7a
Damien Ciabrini <damien.ciabrini@gmail.com>
2022-04-01 18:52:09 +0200

    Add macos build for CI tests
    Closes dciabrin/ngdevkit#74

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eu
-make -s all prefix=$PREFIX GNU_MIRROR=$GNU_MIRROR NEWLIB_MIRROR=$NEWLIB_MIRROR
-sudo make -s install prefix=$PREFIX
+MAKE=$(which gmake make | head -1)
+$MAKE all prefix=$PREFIX GNU_MIRROR=$GNU_MIRROR NEWLIB_MIRROR=$NEWLIB_MIRROR
+sudo $MAKE install prefix=$PREFIX
